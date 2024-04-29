ceffa04a450931386a7078c3c1b812beb6513e05
Bertan Karacora <bertan.karacora@gmail.com>
2024-04-29 17:54:49 +0200

    Add clean option to build script

--- Docker/build.sh
+++ Docker/build.sh
@@ -6,26 +6,42 @@ readonly path_script="$(dirname "$(realpath -s "$0")")"
 source "$path_script/config.sh"
 
 readonly name_image="ids"
+clean=
 
 show_help() {
     echo "Usage:"
-    echo "  ./build.sh"
+    echo "  ./build.sh [--clean]"
     echo
     echo "Build the Docker image."
     echo
 }
 
 parse_args() {
-    if [ "$#" -ne 0 ]; then
-        show_help
-        exit 1
-    fi
+    local arg=""
+    while [[ $# -gt 0 ]]; do
+        arg="$1"
+        shift
+        case $arg in
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        --clean)
+            clean=0
+            ;;
+        *)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        esac
+    done
 }
 
 build() {
     docker build \
         --build-arg CONTAINER_ROS_DOMAIN_ID=$CONTAINER_ROS_DOMAIN_ID \
         --build-arg CONTAINER_BRIDGE_INTERFACE=$CONTAINER_BRIDGE_INTERFACE \
+        ${clean:+--no-cache} \
         --tag "$name_image" \
         --file "$path_script/Dockerfile" \
         "$path_script"
