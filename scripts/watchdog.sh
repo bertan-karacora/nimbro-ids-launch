9965c3c91791d271d503ca324fb68e6a0b121ff2
Bertan Karacora <bertan.karacora@gmail.com>
2024-05-08 15:33:31 +0200

    Setup watchdog (untested)

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -2,42 +2,55 @@
 
 set -euo pipefail
 
-topics=("/camera_ids/ir/image_raw")
-loop_duration=10
-timeout_duration=5
-cmd="/repos/nimbro_orbbec_launch/scripts/start_tmux.sh"
+readonly session_name="ids"
+readonly topics=("/camera_ids")
+readonly duration_loop=10
+readonly duration_timeout=5
 
-check_topics() {
-    for topic in "${TOPICS[@]}"; do
-        if ! timeout --preserve-status $timeout_duration ros2 topic echo --once "$topic" >/dev/null; then
+is_active() {
+    local topic = "$1"
+    local duration_timeout = "$2"
+
+    timeout \
+        --preserve-status "$duration_timeout" \
+        ros2 topic echo --once "$topic" >/dev/null
+
+    return $?
+}
+
+are_active() {
+    for topic in "${topics[@]}"; do
+        if is_active "$topic" "$duration_timeout"; then
+            echo "$topic is active."
+        else
             echo "$topic is silent."
             return 1
-        else
-            echo "$topic has data."
         fi
     done
 
     return 0
 }
 
-main() {
-    while :; do
-        if ! check_topics; then
-            echo "At least one topic is silent for more than $timeout_duration seconds."
+run_watchdog() {
+    if ! are_active; then
+        echo "At least one topic is silent for more than $duration_timeout seconds."
 
-            tmux kill-session -t ids 2>/dev/null
-            if [ $? -ne 0 ]; then
-                echo "Tmux session named ids does not exist or could not be killed. Continuing..."
-            else
-                echo "Tmux session named ids killed."
-            fi
-
-            # Running another script
-            exec "./$LAUNCH_SCRIPT"
-
-            # Wait a bit before restarting the loop
-            sleep 10
+        tmux kill-session -t "$session_name" 2>/dev/null
+        if [ $? -ne 0 ]; then
+            echo "Tmux session named "$session_name" does not exist or could not be killed. Continuing..."
+        else
+            echo "Tmux session named "$session_name" killed."
         fi
+
+        /repos/nimbro_orbbec_launch/scripts/start_tmux.sh
+
+        sleep "$duration_loop"
+    fi
+}
+
+main() {
+    while true; do
+        run_watchdog
     done
 }
 
